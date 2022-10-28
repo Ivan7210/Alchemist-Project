852698cdcd83079e6fd5a75ff9b47ebecbf9a3aa
Meshcheryakov Ivan <50906616+Ivan7210@users.noreply.github.com>
2022-10-28 14:53:41 +0300

    Обновлен оператор сложения

--- ElementsLibrary/Element.cs
+++ ElementsLibrary/Element.cs
@@ -28,27 +28,15 @@ namespace ElementsLibrary
         }
 
         //Скаладывает два элемента 
-        //Э Ф
+        //Б
         public static Element operator + (Element element1, Element element2)
         {
-            if ((element1 == null)||(element2 == null))
-                return null;
-
-            //element1.СheckFormulas()
-
-            foreach (Formula curentFormula in element1.FormulasWithElement)
-            {
-                if (curentFormula.CompareName(element2.Name))
-                    return curentFormula.Result;
-            }
-
-            foreach (Formula curentFormula in element2.FormulasWithElement)
-            {
-                if (curentFormula.CompareName(element1.Name))
-                    return curentFormula.Result;
-            }
-
-            return null;
+            Element result1 = element1.СheckFormulas(element2);
+            Element result2 = element2.СheckFormulas(element1);
+            if (result1 != null)
+                return result1;
+            else
+                return result2;
         }
 
         //Э Ф
@@ -66,10 +54,13 @@ namespace ElementsLibrary
 
         //Проверяет, открыт ли этот элемент игроком, и если не открыт, записывает дату открытия как текущее время
         // Э Б
-        public void OpenElement ()
+        public bool OpenElement ()
         {
             if (OpenDate.CompareTo(new DateTime(1, 1, 1, 0, 0, 0)) == 0)
                 OpenDate = DateTime.Now;
+                return false;
+            else
+                return true;
         }
 
 
